63015743d71785fda5e954ffb8c334d21bc56b06
Son Phan <sonphanusa@gmail.com>
2013-07-08 22:20:41 +0700

    Updated web.js hello index

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var file = "index.html";
+var buffer = fs.readFileSync(file);
 
+var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
