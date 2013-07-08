141f847a5b47b5a9693b8e2d99f8d04d44d051aa
Son Phan <sonphanusa@gmail.com>
2013-07-08 22:26:58 +0700

    Updated web.js hello index

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
 
 var file = "index.html";
-var buffer = fs.readFileSync(file);
+var buffer = new Buffer(16);
+buffer = fs.readFileSync(file);
 
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
